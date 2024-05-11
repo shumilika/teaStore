07dd2d56e36f1b8adc0f4ba330c62d7d419a3651
shumilika <shumilika@gmail.com>
2024-05-12 00:39:37 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { store } from './store';
+import { Provider } from 'react-redux'
 
 
-
-
-
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
   <BrowserRouter>
   <App/>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
+  </Provider>
 );

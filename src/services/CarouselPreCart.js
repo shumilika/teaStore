bb6094a24ddd775992c50d912b6460ac0a14b94f
shumilika <shumilika@gmail.com>
2025-07-02 13:08:52 +0200

    carousel full card fixed

--- src/services/CarouselPreCart.js
+++ src/services/CarouselPreCart.js
@@ -1,93 +1,73 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect,  useState } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { getDownloadURL, getStorage, ref } from 'firebase/storage';
+import { Col, Row } from 'antd';
 
 const CarouselPreCart = (props) => {
 
-    const [nav1, setNav1] = useState(null);
-    const [nav2, setNav2] = useState(null);
-    const [arrayUrl, setArrayUrl] = useState([])
-    let sliderRef1 = useRef(null);
-    let sliderRef2 = useRef(null);
-    
-
-      useEffect(()=>{
-
-        const storage = getStorage()
-
-        const getUrl = (photo) => {
-          return new Promise((resolve, reject) => {
-            getDownloadURL(ref(storage, `img/${photo}`))
-              .then((url) => {
-                resolve(url);
-              })
-              .catch((error) => {
-                reject(error);
-              });
-          });
-        };
-
-        const fetchUrls = async () => {
-          try {
-            const promises = props.imgs.map((photo) => getUrl(photo));
-            setArrayUrl(await Promise.all(promises));
-          } catch (error) {
-            // console.error('Ошибка получения URL-адресов:', error);
-          }
-        };
-        fetchUrls()
-      },[props.imgs])
-
+    const [arrayUrl, setArrayUrl] = useState([]);
 
   useEffect(() => {
-    setNav1(sliderRef1);
-    setNav2(sliderRef2);
-  }, []);
+    const storage = getStorage();
 
-  const styleImg={
-    width:'inherit', height:'400px',
+    const getUrl = (photo) => {
+      return new Promise((resolve, reject) => {
+        getDownloadURL(ref(storage, `img/${photo}`))
+          .then((url) => resolve(url))
+          .catch((error) => reject(error));
+      });
+    };
 
-  }
+    const fetchUrls = async () => {
+      try {
+        const promises = props.imgs.map((photo) => getUrl(photo));
+        const urls = await Promise.all(promises);
+        setArrayUrl(urls);
+      } catch (error) {
+        console.error("Ошибка получения URL-адресов:", error);
+      }
+    };
 
-const styleDivMainImg={
-    height: '400px',
-   width: '100%',
-}
+    fetchUrls();
+  }, [props.imgs]);
 
-    return (
-        <div className="slider-container">
-      
-      <Slider asNavFor={nav2} ref={slider => (sliderRef1 = slider)}>
-      {arrayUrl.map((url, index)=>(
-        
-        <div style={styleDivMainImg} key={index}>
-          <img src={url} alt="" style={styleImg}/>
+  const settings = {
+    customPaging: function (i) {
+      return (
+        <div className="thumb">
+          <img src={arrayUrl[i]} alt={`thumbnail-${i}`} />
         </div>
-      ))}
-      
-      </Slider>
-   
-      <Slider
-        asNavFor={nav1}
-        ref={slider => (sliderRef2 = slider)}
-        slidesToShow={3}
-        swipeToSlide={true}
-        focusOnSelect={true}
-      >
-         {arrayUrl.map((url, index)=>{
-            return(
-            <div key={index}>
-              <img src={url} alt="" width={'120px'} style={{maxHeight:'120px'}}/>
-            </div>
-    );
-        })}
-       
-       
-      </Slider>
-    </div>
-    );
-};
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
+  if (arrayUrl.length === 0) return null; // or loader
+
+  return (
+  <div className='galery-control'>
+  
+          <div className="slider-wrapper">
+      <div className="slider-container">
+        <Slider {...settings}>
+          {arrayUrl.map((url, index) => (
+            <div key={index}>
+              <img src={url} alt={`slide-${index}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+    
+  </div>
+  );
+};
+  
 export default CarouselPreCart;

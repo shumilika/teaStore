9b922d58f277606ace4eacb363fbc07a3ad9b924
shumilika <shumilika@gmail.com>
2025-08-19 16:22:35 +0200

    rebuild routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.scss';
 import Navigation from './components/Navigation';
-import Content from './components/Content';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import { APIProvider } from '@vis.gl/react-google-maps';
@@ -10,6 +9,20 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchFavoritesList, fetchLocalCartList, fethCartList } from './store/personalProduct';
 import { fetchProductList } from './store/products';
+import { Route, Routes } from 'react-router-dom';
+import BillingAddress from './components/Login/BillingAddress';
+import Account from './components/Login/Account';
+import LoginRegister from './components/Login/LoginRegister';
+import Wishlist from './components/Wishlist';
+import ErrorPage from './components/ErrorPage';
+import Cart from './components/Cart/Cart';
+import FullPageCard from './components/Card/FullPageCard';
+import Shop from './components/Shop';
+import ContactUs from './components/ContactUs';
+import FAQS from './components/FAQS';
+import AboutUs from './components/AboutUs';
+import Dashboard from './components/Dashboard';
+import Checkout from './components/Checkout';
 
 
 function App() {
@@ -26,14 +39,36 @@ function App() {
     }
   }, [currentUser, dispatch]);
 
+    const MainLayout = () => (
+    <div className='bgImage'>
+      <Navigation />
+      <div className="content-area">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="about_us" element={<AboutUs />} />
+          <Route path="faqs" element={<FAQS />} />
+          <Route path='contact_us' element={<ContactUs />} />
+          <Route path='shop' element={<Shop />} />
+          <Route path="shop/:id" element={<FullPageCard />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='*' element={<ErrorPage />} />
+          <Route path='wishlist' element={<Wishlist />} />
+          <Route path='account/login' element={<LoginRegister />} />
+          <Route path='account' element={<Account />} />
+          <Route path='account/billing_address' element={<BillingAddress />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
+
   
   return (
     <APIProvider apiKey={API_KEY_MAPS}>
-    <div className='bgImage'>
-    <Navigation/>
-    <Content/>
-    <Footer/>
-    </div>
+     <Routes>
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='/*' element={<MainLayout />} />
+      </Routes>
      </APIProvider>
   );
 }
